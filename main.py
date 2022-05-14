46708b0c745efb513d9300b0d32f5237d335583e
Julien S <73313152+chadow4@users.noreply.github.com>
2022-05-14 12:46:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import string
 import random
+from art import *
+
 
 ## variables qui vont permettrent la génération du mot de passe
 alphabets = list(string.ascii_letters)
@@ -7,21 +9,25 @@ digits = list(string.digits)
 special_characters = list("!@#$%^&*()")
 characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
 
+def header():
+    print("\n▪====================================================================================================================================================▪\n")
+    tprint("Julien  Password   Generator")
+    print("▪====================================================================================================================================================▪\n")
 
 def generate_random_password():
     ## Longueur du mot de passe
-    length = int(input("Longueur du mot de passe: "))
+    length = int(input("● Renseigner la longueur du mot de passe: "))
 
     ## on défini le nombre de lettres, de chiffres et de caractères spéciaux du mot de passe
-    alphabets_count = int(input("Nombre de lettres: "))
-    digits_count = int(input("Nombre de chiffres: "))
-    special_characters_count = int(input("Nombre de caractères spéciaux: "))
+    alphabets_count = int(input("● Nombre de lettres: "))
+    digits_count = int(input("● Nombre de chiffres: "))
+    special_characters_count = int(input("● Nombre de caractères spéciaux: "))
 
     characters_count = alphabets_count + digits_count + special_characters_count
 
     ## Verification si le nombre total de caractères renseigné est plus grand que la longueur du mot de passe
     if characters_count > length:
-        print("Le nombre total de caractères est supérieur à la longueur du mot de passe.")
+        print("● Le nombre total de caractères est supérieur à la longueur du mot de passe.")
         return
 
     ## Initialisation du mot de passe
@@ -51,8 +57,14 @@ def generate_random_password():
 
     ## on convertit le mot de passe en chaine de caractère
     ## et on l'affiche
-    print("".join(password))
+    print("● Votre mot de passe généré :","".join(password))
+
+def credit():
+    print("\nmerci d'avoir utilisé ce générateur :)")
+    print("\ncréateur : https://github.com/chadow4")
 
 
 ## Appel de la fonction
+header()
 generate_random_password()
+credit()
